659bbe1053354ff27eacbdea9bda56a960530256
Gladis NGUIMATIEU <gladis.nguimatieu@arpilabe.com>
2022-02-17 10:04:53 +0100

    ajout des tests xunit

---
+++ ArchiLog/XUnitTestProject/Mock/MockDbContext.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication.Data;
+using XUnitTestProject.Mock.Models;
+
+namespace XUnitTestProject.Mock
+{
+   public  class MockDbContext: EatDbContext
+    {
+        public MockDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public static MockDbContext GetDbContext(bool withData = true)
+        {
+            var options = new DbContextOptionsBuilder().UseInMemoryDatabase("dbtest").Options;
+            var db = new MockDbContext(options);
+
+            if (withData)
+            {
+                db.Customers.Add(new CustomerMock
+                {
+                    Email = "AliAhmadr@yahoo.fr",
+                    Phone = "65421895154",
+                    Lastname = "Charles",
+                    Firstname = "Clementine",
+                    Genre = "Homme",
+                    Address = null,
+                    ZipCode = "6854",
+                    City = "Limoges",
+                });
+
+                db.Customers.Add(new CustomerMock
+                {
+                    Email = "AliAhmadr@yahoo.fr",
+                    Phone = "65421895154",
+                    Lastname = "Charles",
+                    Firstname = "Zan",
+                    Genre = "Homme",
+                    Address = null,
+                    ZipCode = "6854",
+                    City = "Limoges",
+                });
+
+                db.Customers.Add(new CustomerMock
+                {
+                    Email = "AliAhmadr@yahoo.fr",
+                    Phone = "65421895154",
+                    Lastname = "Kaly",
+                    Firstname = "Moli",
+                    Genre = "Femme",
+                    Address = null,
+                    ZipCode = "6854",
+                    City = "Limoges",
+                });
+
+                db.Customers.Add(new CustomerMock
+                {
+                    Email = "AliAhmadr@yahoo.fr",
+                    Phone = "65421895154",
+                    Lastname = "Charles",
+                    Firstname = "Jeep",
+                    Genre = "Femme",
+                    Address = null,
+                    ZipCode = "6854",
+                    City = "Limoges",
+                });
+
+
+                db.SaveChanges();
+            }
+
+            return db;
+        }
+    }
+}
